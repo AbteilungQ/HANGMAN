19950324255af357f00bd8754121d493a82c5386
QbteilungQ <quirin.spam@web.de>
2021-11-13 22:38:39 +0100

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class main {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static String rWord;
+    private static String[] aWord, rAWord;
+    private static int f,r;
+    private static String tipp;
+    private static boolean b=false;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to HANGMAN: \nPlease enter Word: >");
+        rWord = scanner.nextLine().toUpperCase();
+        rAWord = rWord.split("");
+
+        aWord = new String[rWord.length()];
+        for (int i = 0; i < aWord.length; i++) {
+            aWord[i] = "_";
+        }
+
+
+        while (f < 10 && r != aWord.length){
+            System.out.print("Lösungswort: ");
+            for (String s: aWord) {
+                System.out.print(s);
+            }
+            System.out.println("\nTipp: ");
+            b = false;
+            tipp = scanner.nextLine().toUpperCase();
+            for(int i = 0;i < aWord.length; i++){
+                if(rAWord[i].equals(tipp)){
+                    aWord[i] = tipp;
+                    r++;
+                    b = true;
+                }
+            }
+
+            if (!b){
+                f++;
+                System.out.println("Du hast noch "+(10-f)+" Fehlversuche übrig");
+            }
+
+            if (r == rWord.length()){
+                System.out.println("Du hast gewonnen, Glückwunsch ;-)");
+            }else if(f == 10){
+                System.out.println("Du hast verloren, probiere es noch einmal");
+            }
+        }
+
+
+    }
+}
